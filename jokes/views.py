7e2d2cab822d4969d3d32e7974e6c0c30a306f92
Jordy Zuidema <jordy.zuidema@telfort.nl>
2023-01-01 21:09:40 +0100

    execise 13, set up get_absolute_url

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Joke
 
+
+class JokeDetailView(DetailView):
+    model = Joke
+
+
 class JokeListView(ListView):
     model = Joke
+
+
+
 
